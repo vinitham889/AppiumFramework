9b5a193e53ef110be9c026c38f0a81211a325345
vinitham889 <vinitham67@gmail.com>
2024-03-09 22:22:30 +0530

    appium

---
+++ src/main/java/com/appiumframework/utils/AndroidActions.java
@@ -0,0 +1,45 @@
+package com.appiumframework.utils;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
+
+import com.google.common.collect.ImmutableMap;
+
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.android.AndroidDriver;
+
+public class AndroidActions {
+	AndroidDriver driver;
+	public AndroidActions(AndroidDriver driver) {
+		this.driver = driver;
+	}
+	public void longPressAction(WebElement ele)
+	{
+		((JavascriptExecutor)driver).executeScript("mobile : longClickGesture", 
+				ImmutableMap.of("elementId",((RemoteWebElement)ele).getId(),"duration",2000));
+	}
+	public void swipeActions(WebElement ele,String direction) {
+		((JavascriptExecutor)driver).executeScript("mobile : swipeGesture", ImmutableMap.of(
+				"elementId",((RemoteWebElement)ele).getId(),"direction",direction,
+				"percent",0.75));
+	}
+//	public void scrollToEndAction()
+//	{
+//		boolean canScrollMore;
+//		do
+//		{
+//			canScrollMore = (Boolean)((JavascriptExecutor)driver).executeScript("mobile : scrollGesture", ImmutableMap.of(
+//					"left",100,"top",100,"width",200,"height",200,
+//					"direction","down","percent",3.0));
+//			
+//		}while(canScrollMore);
+//	}
+	
+	public void scrollToText(String text)
+	{
+		driver.findElement(MobileBy.AndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\""+text+"\"))"));
+
+	}
+
+}
